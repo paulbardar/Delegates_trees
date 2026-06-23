1f3eb62dfbea3574d7a34d92acb49cc9a0772b9b
paulbardar <Q.C.tulip@gmail.com>
2026-06-23 15:22:18 +0300

    Add project files.

---
+++ Delegate_second/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Delegate_second
+{
+    internal class Program
+    {
+
+        /*
+            Завдання 3
+            Створіть додатки для виконання арифметичних операцій. Підтримувані операції:
+                Перевірка числа на парність;
+                Перевірка числа на непарність;
+                Перевірка чи є число простим;
+                Перевірка чи є число числом Фібоначчі.
+        */
+
+        public delegate void Parity(int x);
+
+        public static void ParityCheck(int x, Parity parity)
+        {
+            parity.Invoke(x);
+        }
+        public static void IsOdd(int x)
+        {
+            if (x % 2 != 0)
+            {
+                Console.WriteLine($"Number {x} is Odd");
+            }
+            else
+            {
+                Console.WriteLine($"Number {x} is Even");
+            }
+        }
+
+        public static void IsEven(int x)
+        {
+            if (x % 2 == 0)
+            {
+                Console.WriteLine($"Number {x} is Even");
+            }
+            else
+            {
+                Console.WriteLine($"Number {x} is Odd");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int x = 4;
+            ParityCheck(x, IsEven);
+
+            int b = 11;
+            ParityCheck(b, IsOdd);
+
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+    
+
+}
---
+++ Delegates_first/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+ Завдання 2
+Створіть додаток для виконання арифметичних операцій. Підтримувані операції:
+    Додавання двох чисел;
+    Віднімання двох чисел;
+    Множення двох чисел.
+Код програми обов'язково повинен використовувати механізм делегатів.
+ */
+
+namespace Delegates_first
+{
+    internal class Program
+    {
+        public delegate int MyDelegate(int a, int b);
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            MyDelegate[] dg = new MyDelegate[]
+            {
+                Add,
+                Subtract,
+                Multiply
+            };
+
+            int x = 2;
+            int y = 6;
+
+            foreach (MyDelegate del in dg)
+            {
+            
+                int result = del(x, y);
+                Console.WriteLine($"{del.Method.Name}({x}, {y}) = {result}");
+            }
+
+            Console.WriteLine("=============================================");
+
+
+            Console.WriteLine(dg[0](2, 6));
+            Console.WriteLine(dg[1](2, 6));
+            Console.WriteLine(dg[2](2, 6));
+
+            
+
+        }
+    }
+}
+
